90a3dd7d5a37c377a47cc4964de2ba6bb2d4274f
pratyush thakur <thakurpratyush225@gmail.com>
2024-08-27 03:29:21 +0530

    ec25

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask ,request , render_template
 import numpy as np 
-
-
 import pandas as pd  
 
 from sklearn.preprocessing import StandardScaler
@@ -45,7 +43,7 @@ def predict_datapoint():
     
     
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", debug = True)
+    app.run(host = "0.0.0.0", port=8080)
         
 
         
